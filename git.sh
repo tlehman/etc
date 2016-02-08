0ae52192f7da2b9caf5bc970b5e825ed044c4913
Tobi Lehman <mail@tobilehman.com>
2016-02-07 16:00:33 -0800

    Add git.sh, bashrc.local and kbd configs

---
+++ git.sh
@@ -0,0 +1,7 @@
+# configure git
+git config --global user.name "Tobi Lehman"
+git config --global user.email "mail@tobilehman.com"
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+git config --global alias.s status
+git config --global alias.a add
+git config --global alias.co checkout
