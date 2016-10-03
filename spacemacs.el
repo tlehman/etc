79fbd7a6e680520ffa31cd667a8a2b005de4ec93
Tobi Lehman <tobi.lehman@gmail.com>
2016-10-03 15:10:18 -0700

    add spacemacs.el

--- spacemacs.el
+++ spacemacs.el
@@ -23,18 +23,22 @@ values."
      ;; Uncomment some layer names and press <SPC f e R> (Vim style) or
      ;; <M-m f e R> (Emacs style) to install them.
      ;; ----------------------------------------------------------------
-     ;; auto-completion
-     ;; better-defaults
+      ;; better-defaults
      emacs-lisp
      git
      haskell
      osx
      markdown
      yaml
+     javascript
+     html
      org
-     (shell :variables
-            shell-default-height 40
-            shell-default-position 'bottom)
+     ruby
+     python
+     tmux
+     scss
+     evil-rails
+     shell
      ;; spell-checking
      ;; syntax-checking
      ;; version-control
@@ -43,7 +47,7 @@ values."
    ;; wrapped in a layer. If you need some configuration for these
    ;; packages, then consider creating a layer. You can also put the
    ;; configuration in `dotspacemacs/user-config'.
-   dotspacemacs-additional-packages '()
+   dotspacemacs-additional-packages '(google-c-style ein wolfram-mode)
    ;; A list of packages and/or extensions that will not be install and loaded.
    dotspacemacs-excluded-packages '()
    ;; If non-nil spacemacs will delete any orphan packages, i.e. packages that
@@ -253,22 +257,56 @@ layers configuration.
 This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place you code here."
-  (load "~/etc/beamer.el")
+  ;(load "~/etc/beamer.el")
   (setq mac-command-modifier 'meta)
+
+  (setq-default truncate-lines 1)
+
+  (defun todays-date ()
+    (shell-command-to-string "echo -n $(date +\"%Y-%m-%d\")"))
+
+  (defun insert-todays-date()
+    (interactive)
+    (insert (todays-date)))
+
+  (defun org-insert-code-block ()
+    (interactive)
+    (insert "#+begin_src \n#+end_src")
+    (dotimes (i 11) (move-point-visually -1)))
+
+  ; Language support in org-babel
+  (require 'ob-ruby)
+  (require 'ob-python)
+  (require 'org-habit)
+  (org-babel-do-load-languages
+   'org-babel-load-languages
+   '((python . t)))
+
+  (display-time)
+  (setq display-time-day-and-date t)
+
+  (defun eww-disable-images ()
+    "Block all images in eww emacs web session"
+    (interactive)
+    (set-buffer "*eww*")
+    (make-local-variable 'shr-blocked-images)
+    (setq shr-blocked-images ".*")
+    (eww-reload))
+
+
   (require 'ansi-color)
   (defun colorize-compilation-buffer ()
     (let ((inhibit-read-only t))
       (ansi-color-apply-on-region (point-min) (point-max))))
   (add-hook 'compilation-filter-hook 'colorize-compilation-buffer)
 
-
   (defun compile-rspec ()
     "run rspec on current file"
     (interactive)
-    (let ((path-name (substring (buffer-file-name) 0 
+    (let ((path-name (substring (buffer-file-name) 0
                                 (string-match "spec/" (buffer-file-name))))
-          (spec-name (substring (buffer-file-name) 
-                                (string-match "spec/" 
+          (spec-name (substring (buffer-file-name)
+                                (string-match "spec/"
                                               (buffer-file-name)))))
       (compile (concat "cd " path-name " && bundle exec rspec -fd " spec-name))))
 
@@ -280,7 +318,20 @@ you should place you code here."
 
   (define-key evil-normal-state-map "tt" 'projectile-toggle-between-implementation-and-test)
   (define-key evil-normal-state-map "\\" 'compile-rspec) 
+
   )
 
 ;; Do not write anything past this comment. This is where Emacs will
 ;; auto-generate custom variable definitions.
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(shr-blocked-images ""))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
