79fb12edd5f1f6f17a2498bb8c4d69183fe2447c
Tobi Lehman <mail@tobilehman.com>
2024-03-22 15:42:15 -0700

    Add emacs.el

--- README.md
+++ README.md
@@ -1,5 +1,31 @@
 # my ~/etc directory
 
+## New tab-oriented emacs config
+Check out [emacs.el](emacs.el), I revamped my emacs config.
+I decided against using spacemacs (too bloated) and even
+evil-mode. My new config is pretty basic, but it adds a
+system for keeping track of notes:
+
+
+### Notes system
+There are two files: ~/Desktop/past.org and ~/Desktop/today.org
+The today file is concatenated to the past file, and then today is
+emptied and set up with today's date. This is done with the `(idempotent-today-move-to-past)` function.
+
+This is a simpler system than my old devlog.org which attempted to keep everything in one file. It also avoids the complexity of org-roam. While I love org and I love org-roam, it was a mess to use, and I already just use Roam for my knowledge graph. I want dumb files.
+
+This is nice because it maintains one file, but the working file is small and minimal.
+
+### Tabs
+The `tab-bar-mode` is the thing I missed most about doing a lot of work in emacs. I could manage projects with projectile, but I need a visual like the tab bar. This hits the spot.
+
+I also made a `Super-t` shortcut that opens a new tab and drops into interactive `find-file`, this is the workflow I like the most for staying in the same frame but opening up another project or file.
+
+### Eshell
+So I've used `shell` and `term` but they are awkward, and if I'm going to use a terminal I will just use a terminal. If I'm staying in emacs, then I'll use all the elisp features built into eshell.
+
+I like VSCode's `Ctrl-backtick` shortcut to open a little shell window below the current file. I emulated that in my config. I also made a global eshell tab you can jump to using `<f4>`, that's the key I mapped on macOS to bring up iTerm. Both these two shortcuts toggle, so you can hit them twice to do a noop. 
+
 This is where I keep some of my config files, and some emacs lisp files.
 
 ## Clojure shortcuts
---
+++ emacs.el
@@ -0,0 +1,136 @@
+;; AUTHOR: tlehman
+;; DATE: 2024-03-22
+;; DESCRIPTION: .emacs file with everything I need, a note-taking system, custom functions and configs
+;;              spacemacs is bloated and I wanted something _fast_er, especially since it's Lent.
+;; 
+;; APPEARANCE
+(set-frame-font "Inconsolata 22")
+(when window-system (set-frame-size (selected-frame) 120 49))
+(global-hl-line-mode)
+(setq inhibit-startup-screen t)
+(load-theme 'aircon t)
+
+;; UTILITY FUNCTIONS
+(defun any? (list)
+  "returns t if list has any non-nil values"
+  (seq-reduce (lambda (a b) (or a b)) list nil))
+
+;; STARTUP
+(tab-bar-mode)
+(server-start)
+;; STARTUP FRAME CONFIG (where, how big?)
+(setq default-frame-alist
+       '((height . 55)
+         (width . 120)
+         (left . 0)
+         (top . 0)
+         (vertical-scroll-bars . nil)
+         (horizontal-scroll-bars . nil)
+         (tool-bar-lines . 0)))
+
+;; CONFIGURATION
+(setq backup-directory-alist
+      '(("." . "~/.emacs.backups.d")))
+(setq evil-default-state 'emacs)
+
+;; KEYBOARD MAPPING
+(defun new-tab-and-find-file ()
+  "Open new tab and drop into find-file"
+  (interactive)
+  (tab-bar-new-tab)
+  (call-interactively 'find-file))
+
+(defun eshell-tab-is-open? ()
+  "Check if eshell tab is currently open"
+  (let ((tab-names (mapcar (lambda (tab) (cdr (car (cdr tab)))) (tab-bar-tabs))))
+    (any? (mapcar (lambda (tab-name) (string= "*eshell*" tab-name)) tab-names))))
+
+(defun new-tab-and-open-eshell ()
+  "open *eshell* tab, or switch to it if it exists, otherwise close the *eshell* tab"
+  (interactive)
+  (if (string= "*eshell*" (buffer-name))
+      (tab-bar-close-tab)
+      (if (eshell-tab-is-open?) 
+	  (tab-bar-switch-to-tab "*eshell*")
+	(progn
+	  (tab-bar-new-tab)
+	  (call-interactively 'eshell)))))
+
+(defun in-situ-eshell ()
+  "vscode refugees LOVE this function"
+  (interactive)
+  (if (string= "*eshell*" (buffer-name))
+      (delete-window)
+    (progn
+      (split-window)
+      (other-window 1)
+      (call-interactively 'eshell t [default-directory]))))
+
+(global-set-key (kbd "s-t") 'new-tab-and-find-file)
+(global-set-key (kbd "s-w") 'tab-bar-close-tab)
+(global-set-key (kbd "<f4>") 'new-tab-and-open-eshell)
+(global-set-key (kbd "C-`") 'in-situ-eshell)
+
+;; NOTES & TIME
+(require 'subr-x)
+(require 'dash)
+(defvar today "~/Desktop/today.org")
+(defvar past "~/Desktop/past.org")
+
+;; NOTES & TIME: today.org functions
+(defun date-now ()  (string-trim (shell-command-to-string "date +'%Y-%m-%d'")))
+(defun today-contents ()
+  "Show the contents of ~/Desktop/today.org"
+  (let ((command (string-join (list "cat" today) " ")))
+    (shell-command-to-string command)))
+(defun today-contents-outdated ()
+  "Check if ~/Desktop/today.org is out of date"
+  (let ((command (list "grep '" (date-now) "' " today " | wc -c")))
+    (-> command
+	(string-join)
+	(string-trim)
+	(shell-command-to-string) ; execute grep today.org | wc -c
+	(string-trim)
+	(string-to-number)        ; get integer line count
+	(= 0)                     ; check if output is empty, if so, then the file is out of date
+	)))
+(defun today-move-to-past ()
+  (shell-command-to-string		; append today.org to past.org
+   "cd ~/Desktop && cat today.org past.org > temp.org && mv temp.org past.org")
+  (shell-command-to-string		; write new today.org for today
+   "printf \"* $(date +'%Y-%m-%d')\n\" > ~/Desktop/today.org"
+   ))
+
+;; NOTES & TIME: move outdated today.org file to past.org, reset today.org for TODAY
+(defun idempotent-today-move-to-past ()
+  (interactive)
+  (if (today-contents-outdated)
+      (today-move-to-past)))
+
+(idempotent-today-move-to-past)
+
+(defun show-today-and-past-notes () 
+  (switch-to-buffer "*scratch*")
+  (split-window-below)
+  (find-file "~/Desktop/today.org")
+  (switch-to-buffer "today.org")
+  (split-window-right)
+  (find-file "~/Desktop/past.org")
+  (other-window 1))
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("e7ce09ff7426c9a290d06531edc4934dd05d9ea29713f9aabff834217dbb08e4" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "8122fb61548fe36171d9cf24cdb9b5f24d053b626d4cda739c3815e080623209" "e80ef6dcd230438866130c6b3b48c05616a5fecfbc5a8169385367723f15fc61" "e9d47d6d41e42a8313c81995a60b2af6588e9f01a1cf19ca42669a7ffd5c2fde" default))
+ '(package-selected-packages
+   '(nlinum aircon-theme spacemacs-theme treemacs cider clojure-mode magit evil-visual-mark-mode dash)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
